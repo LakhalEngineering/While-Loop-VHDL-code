e8fd7ad738739f5355588683807a945d19ddd520
LakhalEngineering <125914042+LakhalEngineering@users.noreply.github.com>
2023-02-26 11:09:09 +0100

    Inti Commit

---
+++ T05_WhileLoopTb.vhd
@@ -0,0 +1,16 @@
+entity T05_WhileLoopTb is
+end entity;
+
+architecture sim of T05_WhileLoopTb is
+begin
+
+        process is
+            variable i : integer :=0;
+            begin
+                while i < 10 loop
+                    report "i = " & integer'image(i);
+                    i := i +2;
+                end loop;
+                wait;
+        end process;
+end architecture;
